9991237ba38d05d79633bdb33992ffe35a1e7bb2
UshaSudulaguntla-11 <ushasudulaguntla@gmail.com>
2026-06-15 10:50:46 +0530

    upgrade to gunicorn and disable debug mode

--- app.py
+++ app.py
@@ -85,8 +85,5 @@ def predict():
         return jsonify({"error": f"Prediction failed: {str(err)}"}), 500
 
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=7860, debug=False)
